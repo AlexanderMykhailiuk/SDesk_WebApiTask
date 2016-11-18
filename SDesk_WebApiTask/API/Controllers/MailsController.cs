2d7fb9a2b24ccc19f0e0af48360fe561469a2a6c
sasha <sashahuan@gmail.com>
2016-11-18 17:58:52 +0200

    Create mailscontroller

---
+++ SDesk_WebApiTask/API/Controllers/MailsController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Epam.Sdesk.Model;
+using static SDSK.API.DBEmulator.EmulatorOfEmailDb;
+
+namespace SDSK.API.Controllers
+{
+    public class MailsController : ApiController
+    {
+        public HttpResponseMessage GetAllMails()
+        {
+            var response = Request.CreateResponse<IEnumerable<Mail>>(HttpStatusCode.OK, Mails);
+
+            return response;
+        }
+
+        public HttpResponseMessage GetMail(int id)
+        {
+            Mail gettingMail = Mails.FirstOrDefault(m => m.Id == id);
+
+            HttpResponseMessage response;
+
+            if (gettingMail !=null) response = Request.CreateResponse(HttpStatusCode.OK, gettingMail);
+            else response = new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            return response;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddMail(Mail newMail)
+        {
+            if(ModelState.IsValid) //<- needing add attributes for mail in class definition
+            {
+
+                newMail.Id = Mails.Count() + 1;
+                Mails.Add(newMail);
+
+                return  new HttpResponseMessage(HttpStatusCode.Created);
+            }
+
+            return  new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateMail(int id, Mail updatedMail)
+        {
+            if(ModelState.IsValid)
+            {
+                Mail oldMail = Mails.FirstOrDefault(m => m.Id == id);
+
+                if (oldMail == null) return  new HttpResponseMessage(HttpStatusCode.NotFound);
+
+                var mapper = new MapperConfiguration( cfg => cfg.CreateMap<Mail,Mail>() ).CreateMapper(); // I prefer to use automapper to update fields of mail
+                mapper.Map(updatedMail, oldMail);
+                oldMail.Id = id;
+
+                return  new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+        public HttpResponseMessage DeleteMail(int id)
+        {
+            Mail deletingMail = Mails.FirstOrDefault(m => m.Id == id);
+
+            HttpStatusCode status;
+
+            if (deletingMail != null)
+            {
+                Mails.Remove(deletingMail);
+                status = HttpStatusCode.NoContent;
+            }
+            else status = HttpStatusCode.NotFound;
+
+            return  new HttpResponseMessage(status);
+        }
+    }
+}
---
+++ SDesk_WebApiTask/API/DBEmulator/EmulatorOfEmailDB.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Epam.Sdesk.Model;
+
+namespace SDSK.API.DBEmulator
+{
+    internal static class EmulatorOfEmailDb
+    {
+        private static List<Mail> _mails;
+
+        internal static List<Mail> Mails
+        {
+            get { return _mails; }
+        }
+
+        static EmulatorOfEmailDb()
+        {
+            _mails = new List<Mail>()
+            {
+                new Mail() { Id = 1, Cc = "Theme1", Body = "Description1"},
+                new Mail() { Id = 2, Cc = "Theme2", Body = "Description2"},
+                new Mail() { Id = 3, Cc = "Theme3", Body = "Description3"}
+            };
+        }
+    }
+}
