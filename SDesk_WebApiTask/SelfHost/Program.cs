8ea287c69d86373b3be4f543ef6da15ac704fe46
AlexanderMykhailiuk <sashahuan@gmail.com>
2016-11-21 23:00:12 +0200

    Self host is added

---
+++ SDesk_WebApiTask/SelfHost/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Http.SelfHost;
+using SDSK.API;
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+            
+            WebApiConfig.Register(config);
+            
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                try
+                {
+                    server.OpenAsync().Wait();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
